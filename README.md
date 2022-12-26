89219963cbdb1d0449ac885b5c5fcf98a710c9f5
icdev2dev <icdev2dev@gmail.com>
2022-12-26 01:41:21 -0800

    second commit

--- README.md
+++ README.md
@@ -1,37 +1,82 @@
-# metacalls
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT THE AUTHORS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-Welcome to your new metacalls project and to the internet computer development community. By default, creating a new project adds this README and some template files to your project directory. You can edit these template files to customize your project and to include your own code to speed up the development cycle.
+# METACALLS -- Calling using derived identities
 
-To get started, you might want to explore the project directory structure and the default configuration file. Working with this project in your development environment will not affect any production deployment or identity tokens.
+This project is inspired by https://forum.dfinity.org/t/icdevs-org-bounty-23b-metacalls-rust-up-to-10k/ post. As mentioned in that post, a canister has, by default, only one identity namely it's principal. Using t-ecsda, it is possible to create multiple derived identities for the same canister. Additionally it is possible to sign messsages using any one of the multiple derived entities; that can be verified. 
 
-To learn more before you start working with metacalls, see the following documentation available online:
+Currently it is possible to : 
 
-- [Quick Start](https://smartcontracts.org/docs/quickstart/quickstart-intro.html)
-- [SDK Developer Tools](https://smartcontracts.org/docs/developers-guide/sdk-guide.html)
-- [Rust Canister Devlopment Guide](https://smartcontracts.org/docs/rust-guide/rust-intro.html)
-- [ic-cdk](https://docs.rs/ic-cdk)
-- [ic-cdk-macros](https://docs.rs/ic-cdk-macros)
-- [Candid Introduction](https://smartcontracts.org/docs/candid-guide/candid-intro.html)
-- [JavaScript API Reference](https://erxue-5aaaa-aaaab-qaagq-cai.raw.ic0.app)
+```
+ dfx canister call metacalls_backend list_messages
+ --> ("")
 
-If you want to start working on your project right away, you might want to try the following commands:
+ dfx canister call metacalls_backend list_derived_identities
+ --> ("")
+ ```
 
-```bash
-cd metacalls/
-dfx help
-dfx canister --help
+
+Then one create a default derived identity (aka root aka /). 
+
+```
+dfx canister call metacalls_backend create_derived_identity /
+dfx canister call metacalls_backend list_derived_identities
+
+--> ("/,")
+
+dfx canister call metacalls_backend get_derived_identity /
+--> (
+  "PIdentity { key_name: \"/\", created_ts: 1672037770528881133, public_key: [3, 160, 144, 177, 117, 224, 24, 16, 10, 47, 253, 36, 97, 241, 126, 123, 179, 168, 69, 216, 35, 125, 11, 2, 22, 81, 110, 15, 15, 255, 104, 162, 153], chain_code: [14, 36, 113, 223, 4, 223, 30, 233, 84, 18, 245, 239, 188, 169, 101, 252, 75, 203, 97, 0, 116, 130, 121, 78, 115, 78, 105, 44, 174, 47, 111, 222] }",
+)
+
 ```
 
-## Running the project locally
+Then one can create and sign messages
 
-If you want to test your project locally, you can use the following commands:
+```
+dfx canister call metacalls_backend create_message "Hello world"
+--> ("5EC52E6D9D0A1349C5EAD8BDD69C438EE9570BBC79231A0CCE35B1F33F76AB96")
 
-```bash
-# Starts the replica, running in the background
-dfx start --background
 
-# Deploys your canisters to the replica and generates your candid interface
-dfx deploy
+dfx canister call metacalls_backend list_messages 
+--> (
+  "Message { uuid: \"5EC52E6D9D0A1349C5EAD8BDD69C438EE9570BBC79231A0CCE35B1F33F76AB96\", created_ts: 1672038035331483194, last_updated_ts: 1672038035331483194, status: Created, original_message: \"Hello world\", hashed_message: [100, 236, 136, 202, 0, 178, 104, 229, 186, 26, 53, 103, 138, 27, 83, 22, 210, 18, 244, 243, 102, 178, 71, 114, 50, 83, 74, 138, 236, 163, 127, 60], signed_by: \"/\", signature: [] }, ",
+)
+
+```
+Notice no signature, yet. 
+
+```
+ dfx canister call metacalls_backend sign_message 5EC52E6D9D0A1349C5EAD8BDD69C438EE9570BBC79231A0CCE35B1F33F76AB96
+
+--> ("ok")
+
 ```
 
-Once the job completes, your application will be available at `http://localhost:4943?canisterId={asset_canister_id}`.
+And we have a signature!
+```
+ dfx canister call metacalls_backend sign_message 5EC52E6D9D0A1349C5EAD8BDD69C438EE9570BBC79231A0CCE35B1F33F76AB96 
+--> (
+  "Message { uuid: \"5EC52E6D9D0A1349C5EAD8BDD69C438EE9570BBC79231A0CCE35B1F33F76AB96\", created_ts: 1672038035331483194, last_updated_ts: 1672038213649756290, status: Signed, original_message: \"Hello world\", hashed_message: [100, 236, 136, 202, 0, 178, 104, 229, 186, 26, 53, 103, 138, 27, 83, 22, 210, 18, 244, 243, 102, 178, 71, 114, 50, 83, 74, 138, 236, 163, 127, 60], signed_by: \"/\", signature: [131, 174, 29, 130, 184, 16, 79, 38, 38, 120, 186, 25, 45, 155, 143, 93, 128, 189, 112, 171, 203, 81, 32, 159, 46, 127, 118, 163, 229, 148, 186, 56, 95, 197, 242, 125, 135, 93, 84, 201, 100, 19, 30, 49, 247, 142, 168, 81, 130, 197, 223, 11, 40, 166, 86, 133, 167, 185, 32, 77, 87, 4, 125, 198] }, ",
+)
+
+```
+## DOS 
+Limited defensive denial of service capabilities provided through dos. 
+
+## 1
+The function "create_signed_message" creates a message and submits it for automatic signing. 
+
+## 2
+The function "create_message_for" is similar to create_message; except that it creates a message for a specific identity (i.e /finance/user/alice) for signature. The sign_message assumes the identity of /finance/user/alice).
+
+## 3
+The function "create_signed_message_with" is similar to create_signed_message; except that it creates a message and submits it for automatic signing with the specific derived identity (i.e. /engg/user/bob)
+
+## 4 
+The function "mc_set_timer_interval" sets the timing interval in seconds for message_queue to be checked for pruning. Initially 10 secs
+
+## 5 
+The function "mc_set_time_to_archive_message" sets the time from the create_date that the messages will be pruneds in ns. Initially 10000000000 ns
+
+
+
--- src/metacalls_backend/src/lib.rs
+++ src/metacalls_backend/src/lib.rs
@@ -1,4 +1,629 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::borrow::BorrowMut;
+use std::{cell::RefCell, fmt::Debug};
+use sha2::{Digest, Sha256};
+use std::collections::BTreeMap;
+use data_encoding::HEXUPPER;
+
+use std::time::Duration;
+use ic_cdk::api::management_canister::ecdsa::{ecdsa_public_key, EcdsaPublicKeyArgument, EcdsaKeyId, sign_with_ecdsa, SignWithEcdsaArgument, SignWithEcdsaResponse};
+use ic_cdk::id;
+use ic_cdk::timer::TimerId;
+
+
+mod dos;
+use dos::{guard_query_function, dos_set_timer_interval};
+const DOS_INIT_FUNCTION:&str = "init_function";
+
+const INIT_MC_TIMER_INTERVAL:u8 = 10;
+const INIT_MC_TIME_TO_ARCHIVE:u64 = 10000000000;
+
+
+const DEV_KEY_NAME:&str = "dfx_test_key";
+const DEV_SIGN_CYCLES:u64 = 0;
+const DEV_FILE_NAME:&str = "dev/pidentities";
+const PROD_KEY_NAME:&str = "key_1";
+const PROD_SIGN_CYCLES:u64 = 26_153_846_153;
+const PROD_FILE_NAME:&str = "prod/pidentities";
+
+
+thread_local! {
+    static MESSAGES:RefCell<Messages> = RefCell::default();
+    static PIDENTITIES:RefCell<PIdentities> = RefCell::default();
+    static MC_TIMER_INTERVAL:RefCell<u8> = RefCell::new(INIT_MC_TIMER_INTERVAL);
+    static MC_TIMER:RefCell<TimerId> = RefCell::default();
+    static MC_TIME_TO_ARCHIVE:RefCell<u64> = RefCell::new(INIT_MC_TIME_TO_ARCHIVE);
+
 }
+
+
+pub async fn user_init_function(_arg:String) -> () {
+    create_derived_identity(String::from("/")).await;
+    dos_set_timer_interval(String::from("100")).await;
+
+
+    MC_TIMER_INTERVAL.with(|refcell| {
+        let timer_id = ic_cdk::timer::set_timer_interval(Duration::from_secs(*refcell.borrow() as u64), || ic_cdk::spawn(message_checker()));
+        MC_TIMER.with(move |refcell| {
+            refcell.replace(timer_id);
+        })
+    }); 
+
+}
+
+
+
+#[ic_cdk_macros::update]
+async fn mc_set_time_to_archive_message(interval: String) -> String{
+    let interval = interval.parse::<u64>().unwrap();
+    MC_TIME_TO_ARCHIVE.with(|refcell| {
+        refcell.replace(interval);
+    });
+    "ok".to_owned()
+}
+
+#[ic_cdk_macros::update]
+pub async fn mc_set_timer_interval(interval: String) -> String {
+
+    let interval = interval.parse::<u64>().unwrap();
+    MC_TIMER.with( |refcell| {
+        let timer_id = &mut *refcell.borrow_mut();
+        ic_cdk::timer::clear_timer(*timer_id);
+ 
+    });
+
+    MC_TIMER.with(move |refcell| {
+        let timer_id = ic_cdk::timer::set_timer_interval(Duration::from_secs(interval), || ic_cdk::spawn(message_checker()));
+        refcell.replace(timer_id);
+    });
+
+   
+    format!("ok")
+}
+
+
+
+
+async fn message_checker() {
+
+    let current_ts = ic_cdk::api::time();
+
+    MESSAGES.with(|refcell| {
+
+        let messages = &mut *refcell.borrow_mut();
+
+        let  x= &mut *messages.messages.borrow_mut();
+        let mut keys_to_remove:Vec<String> = vec![];
+
+        MC_TIME_TO_ARCHIVE.with (|refcell| {
+
+            let time_to_archive = &*refcell.borrow();
+            let time_to_archive = time_to_archive.clone();
+
+            for key in x.keys() {
+                let message = x.get(key).unwrap();
+                if current_ts - message.get_created_ts() > time_to_archive {
+                    keys_to_remove.insert(0, String::from(key));
+
+                }
+            }
+        });
+
+        for key in keys_to_remove {
+            x.remove(&key);
+        }
+
+    });
+
+}
+
+
+
+#[ic_cdk_macros::update]
+async fn create_message (original_message: String) -> String {
+    if let Some(uuid) = ic_cdk::api::management_canister::main::raw_rand().await.ok() {        
+        if let Some(message) = Message::new(&original_message, "/",  uuid) {
+           return String::from(message.get_uuid())
+        }
+        else {
+            return "".to_owned();
+        }
+    } 
+    else {
+        return "".to_owned();
+    }   
+}
+
+
+
+#[ic_cdk_macros::update]
+async fn create_message_for (original_message: String, signed_by: String) -> String {
+    if let Some(uuid) = ic_cdk::api::management_canister::main::raw_rand().await.ok() {        
+        if let Some(message) = Message::new(&original_message, &signed_by,  uuid) {
+           return String::from(message.get_uuid())
+        }
+        else {
+            return "".to_owned();
+        }
+    } 
+    else {
+        return "".to_owned();
+    }   
+}
+
+
+
+
+#[ic_cdk_macros::update]
+async fn sign_message (uuid: String) -> String {
+
+    let mut message_hash = vec![];
+    let mut derivation_path = vec![];
+    
+    (message_hash, derivation_path) =      MESSAGES.with(|refcell| {
+        
+        let messages = & *refcell.borrow();
+
+        let x = & mut *messages.messages.borrow_mut();
+        let message = x.get(&uuid);
+    
+        if let Some(message) = message {
+            ( message.hashed_message.clone(), vec![message.signed_by.clone().into_bytes()])
+        }
+        else {
+            (vec![], vec![])
+        }
+            
+    });
+
+    let key_id = EcdsaKeyId {
+        curve: ic_cdk::api::management_canister::ecdsa::EcdsaCurve::Secp256k1,
+        name: String::from(PIdentities::get_environment().key_name),
+    };
+
+
+    let arg = SignWithEcdsaArgument { 
+        message_hash,
+        derivation_path,
+        key_id, 
+    };
+
+    let signature = sign_with_ecdsa(arg).await.ok();
+
+    if let Some(signature) = signature {
+        let signature = signature.0.signature;
+
+
+        MESSAGES.with(move |refcell| {
+    
+            let messages = &mut *refcell.borrow_mut();
+
+            let x = &mut *messages.messages.borrow_mut();
+
+            let message = x.get(&uuid);
+            
+            if let Some(message) = message {
+                let mut cloned_message = message.clone();
+    
+                let last_updated_ts = ic_cdk::api::time();
+    
+                let message_hash = message.hashed_message.clone();
+                let derivation_path = vec![message.signed_by.clone().into_bytes()];
+    
+    
+                cloned_message.set_last_updated_ts(last_updated_ts);
+                cloned_message.set_status(MessageStatus::Signed);
+                cloned_message.set_signature(signature);
+
+    
+                x.insert(uuid, cloned_message);
+                "ok".to_owned()
+            }
+            else {
+                "".to_owned()
+            }        
+        })
+
+    } 
+    else {
+        "".to_owned()
+    }
+    
+}
+
+
+ 
+#[ic_cdk_macros::update]
+async fn create_signed_message_with(original_message:String, signed_by: String) -> String {
+
+    let uuid = create_message_for(original_message, signed_by).await;
+    if uuid.eq_ignore_ascii_case("") {
+        "".to_owned()
+    }
+    else {
+        let cloned_uuid = uuid.clone();
+
+        let result = sign_message(uuid).await;
+        if result.eq_ignore_ascii_case("") {
+            "".to_owned()
+        }
+        else {
+            cloned_uuid
+        }
+
+    } 
+
+}
+
+
+
+#[ic_cdk_macros::update]
+async fn create_signed_message(original_message:String) -> String {
+
+    let uuid = create_message(original_message).await;
+    if uuid.eq_ignore_ascii_case("") {
+        "".to_owned()
+    }
+    else {
+        let cloned_uuid = uuid.clone();
+
+        let result = sign_message(uuid).await;
+        if result.eq_ignore_ascii_case("") {
+            "".to_owned()
+        }
+        else {
+            cloned_uuid
+        }
+
+    } 
+
+}
+
+
+
+
+#[ic_cdk_macros::query (guard="guard_query_function")]
+fn list_messages() -> String {
+
+    MESSAGES.with(|refcell| {
+        let mut ret_str = String::from("");
+
+        let messages = &*refcell.borrow();
+
+        let x = &*messages.messages.borrow();
+
+        for key in x.keys() {
+            let message = x.get(key).unwrap();
+            let string_message = format!("{:?}, ", message);
+
+            ret_str.push_str(&string_message);
+
+
+        }
+        ret_str
+    })    
+}
+
+
+
+#[ic_cdk_macros::update]
+async fn create_derived_identity(key_name:String) -> String {
+    let derived_identity = PIdentity::new_derived_identity(key_name).await;
+    derived_identity.get_key_name()
+}
+
+#[ic_cdk_macros::query(guard="guard_query_function")]
+fn list_derived_identities() -> String {
+    let v1= PIdentities::get_derived_identities();
+    let mut ret_str = String::new();
+    for i in v1 {
+        ret_str.push_str(&format!("{},", i));
+    }
+    ret_str
+}
+
+
+#[ic_cdk_macros::query(guard="guard_query_function")]
+fn get_derived_identity(key_name:String) -> String {
+    if let Some(pid) = PIdentities::get_derived_identity(key_name) {
+            format!("{:?}", pid)
+    }
+    else {
+        "".to_owned()
+    }
+}
+
+
+
+#[derive(Default, Debug, Clone)]
+pub enum EnvironmentType {
+    #[default] Dev,
+    Prod,
+}
+
+#[derive( Debug, Clone)]
+struct Environment {
+    environment_type: EnvironmentType,
+    key_name: String, 
+    sign_cycles: u64,
+    file_name: String,
+}
+
+impl Default for Environment {
+    fn default() -> Self {
+        let environment_type = EnvironmentType::default();
+        Environment::from(environment_type)
+    }
+}
+
+
+impl From<EnvironmentType> for Environment {
+    fn from(environment_type: EnvironmentType) -> Self {
+        match environment_type {
+            EnvironmentType::Dev => {
+                Self {
+                    environment_type,
+                    key_name: DEV_KEY_NAME.to_owned(),
+                    sign_cycles: DEV_SIGN_CYCLES,
+                    file_name: DEV_FILE_NAME.to_owned(),
+                }
+            },
+            EnvironmentType::Prod => {
+                Self {
+                    environment_type,
+                    key_name: PROD_KEY_NAME.to_owned(),
+                    sign_cycles: PROD_SIGN_CYCLES,
+                    file_name: PROD_FILE_NAME.to_owned(),
+                }
+            },
+        }       
+    }
+}
+
+
+
+#[derive(Debug, Default)]
+pub struct PIdentities {
+    pidentities: BTreeMap<String, PIdentity>,
+    environment: Environment,
+}
+
+
+impl PIdentities {
+    fn set_environment (&mut self, environment: Environment) {
+        self.environment = environment;
+    }
+
+    fn get_environment () -> Environment{
+        PIDENTITIES.with(|refcell| {
+            let pids = &*refcell.borrow();
+            pids.environment.clone()
+        })
+    }
+}
+impl PIdentities {
+
+    pub fn switch_environment (environment_type: EnvironmentType) {
+
+        PIDENTITIES.with(|refcell| {
+            let pidentities = &mut *refcell.borrow_mut();
+            pidentities.set_environment(Environment::from(environment_type));
+
+        })
+    }
+
+    pub fn get_derived_identities() -> Vec<String> {        
+        PIDENTITIES.with( |refcell|{ 
+            let pidentities = &*refcell.borrow();
+            pidentities.pidentities.keys().cloned().collect()
+            
+        })
+    }
+
+    pub fn get_derived_identity (key_name:String) -> Option<PIdentity> {
+        
+        PIDENTITIES.with( |refcell|{ 
+            let pidentities = &*refcell.borrow();
+            let pid = pidentities.pidentities.get(&key_name);
+            match  pid {
+                Some(pid) => {
+                    let clone_pid = pid.clone();
+                    Some(clone_pid)
+                },
+                None => None,
+            }
+        })
+    }
+}
+
+
+#[derive(Debug, Default, Clone)]
+
+pub struct PIdentity {
+    key_name: String,
+    created_ts: u64,
+    public_key: Vec<u8>,
+    chain_code: Vec<u8>,
+}
+
+impl PIdentity {
+    pub fn get_key_name (&self) -> String {
+        self.key_name.clone()
+    }
+    pub fn _get_created_ts(&self) -> u64 {
+        self.created_ts
+    }
+
+    fn _get_public_key(&self) -> Vec<u8> {
+        self.public_key.clone()
+    }
+    fn _get_chain_code(&self) -> Vec<u8> {
+        self.chain_code.clone()
+    }
+}
+
+
+impl PIdentity {    
+
+    pub async fn new_derived_identity(key_name:String) -> Self {
+
+        let cloned_key_name= key_name.clone();
+        let cloned_key_name2= key_name.clone();
+        
+
+        let created_ts = ic_cdk::api::time();
+
+        let key_id = EcdsaKeyId {
+            curve: ic_cdk::api::management_canister::ecdsa::EcdsaCurve::Secp256k1,
+            name: String::from(PIdentities::get_environment().key_name),
+        };
+
+
+
+        let canister_id = Some(id());
+        let derivation_path = vec![key_name.into_bytes()];
+
+
+        let arg = EcdsaPublicKeyArgument { 
+            canister_id, 
+            derivation_path, 
+            key_id
+        };
+
+        let r =  ecdsa_public_key(arg).await.expect("some error");
+
+
+        
+        let public_key = r.0.public_key;
+        let chain_code = r.0.chain_code;
+
+        let new_identity = Self { key_name: cloned_key_name, created_ts, public_key, chain_code};
+        let cloned_new_identity = new_identity.clone();
+
+
+        PIDENTITIES.with(move |refcell|{
+            let pidentities = &mut *refcell.borrow_mut();
+
+            (*pidentities).pidentities.insert(cloned_key_name2, new_identity);
+            cloned_new_identity
+        })        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#[derive(Clone, Debug)]
+enum MessageStatus {
+    Created,
+    Signed,
+    _Sent,     
+}
+
+
+#[derive(Debug, Default)]
+struct Messages {
+    messages:RefCell<BTreeMap<String, Message>>,
+}
+
+
+#[derive(Clone, Debug)]
+struct Message {
+
+    uuid: String,
+    created_ts: u64,
+    last_updated_ts: u64,
+    status: MessageStatus,
+
+    original_message: String,
+    hashed_message: Vec<u8>,
+    signed_by: String,
+    signature: Vec<u8>,
+
+}
+
+
+fn hash_message(original_message:&str) -> Vec<u8> {
+    let mut s = Sha256::new();
+    s.update(original_message.as_bytes());
+    let r = s.finalize();
+    let hashed_message = r.to_vec();
+    hashed_message
+}
+
+
+
+impl Message {
+
+
+    fn new(original_message: &str, signed_by:&str, uuid: (Vec<u8>, )) -> Option<Self> {
+
+        
+        let uuid= uuid.0;
+        let uuid = HEXUPPER.encode(&uuid);
+        let cloned_uuid = uuid.clone();
+
+            
+        MESSAGES.with(move |refcell| {
+
+            let created_ts = ic_cdk::api::time();
+
+            let hashed_message = hash_message(original_message);
+
+            let a = Self { 
+                uuid,
+                created_ts,
+                last_updated_ts: created_ts,
+                status: MessageStatus::Created,
+
+                original_message: String::from(original_message),
+                hashed_message,
+                signed_by: signed_by.to_owned(),
+                signature: vec![],
+
+            };
+            let cloned_a = a.clone();
+            let  messages = &mut *refcell.borrow_mut();
+
+            let x = &mut *messages.messages.borrow_mut();
+
+            x.insert(cloned_uuid, a);
+            
+
+            Some(cloned_a)
+        })
+    }        
+}
+
+
+impl Message {
+    pub fn get_uuid(&self) -> &str {
+        &self.uuid
+    }
+
+    pub fn get_created_ts(&self) -> u64 {
+        self.created_ts
+    }
+    pub fn set_status(&mut self, status:MessageStatus) {
+        self.status = status;
+
+    }
+
+    pub fn set_last_updated_ts (&mut self, last_updated_ts:u64)  {
+        self.last_updated_ts = last_updated_ts;
+    }
+
+    pub fn set_signature(&mut self, signature: Vec<u8>) {
+        self.signature = signature;
+
+    }
+}
+
+
